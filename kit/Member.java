29cf8199efd4bc4cfb73c9b159df5f2e2f8de4e0
Kit <99901278+swiftscout98@users.noreply.github.com>
2023-09-14 02:02:42 +0800

    Update Member.java

--- kit/Member.java
+++ kit/Member.java
@@ -1,38 +1,33 @@
-import java.util.Arrays;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Member extends Person {
     // Data fields
-    private String memberType;
     private int points;
-    private Date regDate;
     private static int totalMember = 0;
-
+    
     // No-argument constructor
     public Member() {
         totalMember++;
     }
-
+    
     // Constructor with corrected parameter types
-    public Member(String ID, Name name, char gender, String phone, Address[] address, String memberType, int points, Date regDate, int totalMember) {
-        super(ID, name, gender, phone, address);
-        this.memberType = memberType;
+    public Member(String name, char gender, String phone, Address address, int points, int totalMember) {
+        //auto generated date
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HHmm");
+        String orderID = String.format("%sMem%s", dateFormat.format(date),timeFormat.format(date));
+        super(name, gender, phone, address);
         this.points = points;
-        this.regDate = regDate;
         Member.totalMember++;
     }
-
+    
     // Setter methods (setTotalMember corrected)
-    public void setMemberType(String memberType) {
-        this.memberType = memberType;
-    }
-
     public void setPoints(int points) {
         this.points = points;
     }
 
-    public void setRegDate(Date regDate) {
+    public void setRegDate(String regDate) {
         this.regDate = regDate;
     }
 
@@ -42,15 +37,11 @@ public class Member extends Person {
 
     // Getter methods
 
-    public String getMemberType() {
-        return memberType;
-    }
-
     public int getPoints() {
         return points;
     }
 
-    public Date getRegDate() {
+    public String getRegDate() {
         return regDate;
     }
 
@@ -58,19 +49,23 @@ public class Member extends Person {
         return totalMember;
     }
 	
-	//validation
-	//membertype
-	
-    @Override
-    public String toString() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); // Adjust the date format as needed
 
-        return String.format("%-30s %-15s %-15s %-10s %-10s",
-                memberType, points, dateFormat.format(regDate), totalMember);
+    public void addPoint(double totalPrice){
+        points += (int)totalPrice; 
     }
 
-    public static String getHeaderRow() {
-        return String.format("%-30s %-15s %-15s %-10s %-10s",
-                "Member Type", "Points", "Registration Date", "Total Members");
+    public void minusPoint(int minus){
+        points -= minus;
+    }
+
+    //toString
+    public String toString() {
+        return String.format("%s\t\t\nPoints :%s\t\t\nRegister Date: %s",
+                super.toString(),points, regDate);
+    }
+
+    @Override
+    public String getRow() {
+        return String.format("\t\t%-10s %-20s %-15d %-15s",super.ID, super.name, points, regDate);
     }
 }
